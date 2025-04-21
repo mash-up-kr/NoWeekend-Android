05be78861ee4e0f4a9eb4d5bedf9366644bd537a
SEO-J17 <qw4643987@naver.com>
2025-04-22 00:06:32 +0900

    [#2] 모듈 별 의존성 추가 및 플러그인, 라이브러리 적용

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,59 +1,41 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.team.noweekend.android.application)
+    alias(libs.plugins.team.noweekend.android.application.compose)
+    alias(libs.plugins.team.noweekend.hilt)
 }
 
 android {
-    namespace = "team.noweekend"
-    compileSdk = 35
-
+    namespace = "team.noweekend.app"
+    
     defaultConfig {
-        applicationId = "team.noweekend"
-        minSdk = 24
-        targetSdk = 35
+        applicationId = "team.noweekend.app"
         versionCode = 1
-        versionName = "1.0"
-
+        versionName = "0.0.1" // X.Y.Z; X = Major, Y = minor, Z = Patch level
+        
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
-    buildFeatures {
-        compose = true
     }
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
+    // Core modules
+    implementation(project(":core:common-android"))
+    implementation(project(":core:common-kotlin"))
+    implementation(project(":core:common-ui"))
+    implementation(project(":core:data"))
+    implementation(project(":core:domain"))
+    implementation(project(":core:local"))
+    implementation(project(":core:model"))
+    implementation(project(":core:navigator"))
+    implementation(project(":core:remote"))
+    implementation(project(":core:resource"))
+    
+    // Feature modules
+    implementation(project(":feature:home"))
+    
     implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.navigation.compose)
+    implementation(libs.hilt.navigation.compose)
 }
