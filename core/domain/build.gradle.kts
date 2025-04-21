2511a18f319e7612c54910e81300b820df5c236c
SEO-J17 <qw4643987@naver.com>
2025-04-22 00:04:15 +0900

    [#2] domain 모듈 구현

---
+++ core/domain/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    id("kotlin")
+    alias(libs.plugins.team.noweekend.jvm.library)
+}
+dependencies {
+    implementation(project(":core:common-kotlin"))
+    implementation(project(":core:model"))
+    
+    implementation(libs.coroutine.core)
+    implementation(libs.javax.inject)
+}
