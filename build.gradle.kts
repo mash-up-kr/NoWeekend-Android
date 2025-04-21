db24976ae2d51e3139ed72ed20861b85ac31c81a
SEO-J17 <qw4643987@naver.com>
2025-04-22 00:07:00 +0900

    [#2] 필요한 플러그인 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,17 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.jetbrains.kotlin.jvm) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.room) apply false
+    alias(libs.plugins.kotlinx.serialization) apply false
+    alias(libs.plugins.ktlint)
+}
+
+allprojects {
+    apply {
+        plugin(rootProject.libs.plugins.ktlint.get().pluginId)
+    }
 }
