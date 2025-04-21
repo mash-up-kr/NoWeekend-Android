d861b5c9e5758bd84951a52a952cc0831a635462
SEO-J17 <qw4643987@naver.com>
2025-04-22 00:02:16 +0900

    [#2] build-logic 모듈 구현

---
+++ build-logic/convention/src/main/java/AndroidApplicationConventionPlugin.kt
@@ -0,0 +1,31 @@
+import com.android.build.api.dsl.ApplicationExtension
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import team.noweekend.convention.configureKotlinAndroid
+
+class AndroidApplicationConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            with(pluginManager) {
+                apply("com.android.application")
+                apply("org.jetbrains.kotlin.android")
+            }
+            
+            extensions.configure<ApplicationExtension> {
+                buildTypes {
+                    release {
+                        isMinifyEnabled = false
+                        proguardFiles(
+                            getDefaultProguardFile("proguard-android-optimize.txt"),
+                            "proguard-rules.pro"
+                        )
+                    }
+                }
+                
+                configureKotlinAndroid(this)
+                defaultConfig.targetSdk = 35
+            }
+        }
+    }
+}
---
+++ build-logic/convention/src/main/java/AndroidFeatureConventionPlugin.kt
@@ -0,0 +1,35 @@
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+import team.noweekend.convention.libs
+
+class AndroidFeatureConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            pluginManager.apply {
+                apply("team.noweekend.android.library")
+                apply("team.noweekend.android.compose")
+                apply("team.noweekend.hilt")
+            }
+            
+            dependencies {
+                add("implementation", project(":core:common-ui"))
+                add("implementation", project(":core:common-android"))
+                add("implementation", project(":core:common-kotlin"))
+                add("implementation", project(":core:domain"))
+                add("implementation", project(":core:model"))
+                add("implementation", project(":core:resource"))
+                
+                add("implementation", libs.findLibrary("androidx-activity-compose").get())
+                add("implementation", libs.findLibrary("androidx-core-ktx").get())
+                add("implementation", libs.findLibrary("androidx-appcompat").get())
+                add("implementation", libs.findLibrary("hilt-navigation-compose").get())
+                add("implementation", libs.findLibrary("coil").get())
+                add("implementation", libs.findBundle("coroutine").get())
+                add("implementation", libs.findLibrary("navigation-compose").get())
+                add("implementation", libs.findLibrary("androidx-material3").get())
+                add("implementation", libs.findLibrary("androidx-lifecycle-runtime-ktx").get())
+            }
+        }
+    }
+}
