662058ea662324db3c47ad19b5dc9fd14d2eca14
SEO-J17 <qw4643987@naver.com>
2025-04-22 00:07:49 +0900

    [#2] 빌드로직 모듈 설정, 모듈 include

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,7 @@
 pluginManagement {
+    includeBuild("build-logic")
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        google()
         mavenCentral()
         gradlePluginPortal()
     }
@@ -20,4 +15,16 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "NoWeekend"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 include(":app")
+include(":core:data")
+include(":core:model")
+include(":core:navigator")
+include(":core:resource")
+include(":core:common-android")
+include(":core:common-kotlin")
+include(":core:common-ui")
+include(":core:local")
+include(":core:remote")
+include(":feature:home")
+include(":core:domain")
