60135e42aec342f40b1f04695d01797e415626ea
SEO-J17 <qw4643987@naver.com>
2025-04-22 00:05:20 +0900

    [#2] resource 모듈 구현

---
+++ core/resource/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    alias(libs.plugins.team.noweekend.android.library)
+}
+
+android {
+    namespace = "team.noweekend.core.resource"
+}
+
+dependencies {
+    implementation(project(":core:common-android"))
+    implementation(project(":core:common-kotlin"))
+    implementation(project(":core:common-ui"))
+    implementation(project(":core:model"))
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.material3)
+}
