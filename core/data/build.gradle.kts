92997ee9cb15bc8a4f32d3348403377ef4ebdfca
SEO-J17 <qw4643987@naver.com>
2025-04-22 00:04:02 +0900

    [#2] data 모듈 구현

---
+++ core/data/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    alias(libs.plugins.team.noweekend.android.library)
+    alias(libs.plugins.team.noweekend.android.room)
+    alias(libs.plugins.team.noweekend.hilt)
+}
+
+android {
+    namespace = "team.noweekend.core.data"
+}
+
+dependencies {
+    implementation(project(":core:common-android"))
+    implementation(project(":core:common-kotlin"))
+    implementation(project(":core:model"))
+    implementation(project(":core:remote"))
+    implementation(project(":core:local"))
+    implementation(project(":core:domain"))
+    
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    // ktor
+    implementation(libs.bundles.network)
+    // coroutine
+    implementation(libs.bundles.coroutine)
+}
