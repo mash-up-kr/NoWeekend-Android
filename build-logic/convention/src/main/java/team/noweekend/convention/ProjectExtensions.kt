29bbcf2f65eaffb93f195d49429b169de8beff4f
이재성_Kyle <jaesung.lee@lulumedic.com>
2025-04-25 19:56:04 +0900

    [#1] toml findVersion 확장함수 추가

--- build-logic/convention/src/main/java/team/noweekend/convention/ProjectExtensions.kt
+++ build-logic/convention/src/main/java/team/noweekend/convention/ProjectExtensions.kt
@@ -5,5 +5,11 @@ import org.gradle.api.artifacts.VersionCatalog
 import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.getByType
 
-val Project.libs
+internal val Project.libs
     get(): VersionCatalog = extensions.getByType<VersionCatalogsExtension>().named("libs")
+
+internal fun Project.findVersion(versionName: String): String {
+    return checkNotNull(libs.findVersion(versionName).get().toString()) {
+        "Version '$versionName' does not exist in the libs.versions.toml file."
+    }
+}
