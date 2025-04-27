cc41d52b813197eff8308530ff3f8067c5382873
Jaesung Lee <viki0114@naver.com>
2025-04-27 21:38:17 +0900

    [#4] feature:sample 모듈 및 SampleActivity 추가

---
+++ feature/sample/src/main/java/team/noweekend/feature/sample/screen/SampleScreen.kt
@@ -0,0 +1,25 @@
+package team.noweekend.feature.sample.screen
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun SampleScreen(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+        Text("Welcome to the Sample Screen")
+    }
+}
+
+@Preview
+@Composable
+private fun SampleScreenPreview() {
+    SampleScreen(modifier = Modifier.fillMaxSize())
+}
