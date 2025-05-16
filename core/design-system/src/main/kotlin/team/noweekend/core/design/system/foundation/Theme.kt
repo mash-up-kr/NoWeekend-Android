f6b978f98d531475ba52dfed51a1fb06e7e53678
이재성_Kyle <jaesung.lee@lulumedic.com>
2025-05-16 15:40:35 +0900

    [#11] design system 모듈 추가

---
+++ core/design-system/src/main/kotlin/team/noweekend/core/design/system/foundation/Theme.kt
@@ -0,0 +1,10 @@
+package team.noweekend.core.design.system.foundation
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun NoWeekendTheme(
+    content: @Composable () -> Unit
+) {
+    content()
+}
--- core/design-system/src/main/kotlin/team/noweekend/core/design/system/foundation/Typography.kt
+++ core/design-system/src/main/kotlin/team/noweekend/core/design/system/foundation/Typography.kt
@@ -1,4 +1,4 @@
-package team.noweekend.feature.home.theme
+package team.noweekend.core.design.system.foundation
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
