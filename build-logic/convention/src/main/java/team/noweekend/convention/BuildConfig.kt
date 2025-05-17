b62c15f9f8e2da2700d8f0666baba7364244a41b
이재성_Kyle <jaesung.lee@lulumedic.com>
2025-05-17 19:09:04 +0900

    [#1] application id 수정

--- build-logic/convention/src/main/java/team/noweekend/convention/BuildConfig.kt
+++ build-logic/convention/src/main/java/team/noweekend/convention/BuildConfig.kt
@@ -5,7 +5,7 @@ import org.gradle.api.Project
 
 internal fun ApplicationExtension.configureBuildConfig(project: Project) {
     defaultConfig {
-        applicationId = "team.noweekend"
+        applicationId = "team.noweekend.app"
         targetSdk = project.findVersion("targetSdk").toInt()
         versionCode = project.findVersion("versionCode").toInt()
         versionName = project.findVersion("versionName")
