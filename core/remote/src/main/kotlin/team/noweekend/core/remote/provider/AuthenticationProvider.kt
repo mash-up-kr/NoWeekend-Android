6a6350247b8f534d8d22474f818cfee47f7bdd10
SEO-J17 <qw4643987@naver.com>
2025-05-21 23:25:16 +0900

    [#12] 네트워크 auth provider 구현

---
+++ core/remote/src/main/kotlin/team/noweekend/core/remote/provider/AuthenticationProvider.kt
@@ -0,0 +1,28 @@
+package team.noweekend.core.remote.provider
+
+import io.ktor.client.plugins.auth.AuthProvider
+import io.ktor.client.request.HttpRequestBuilder
+import io.ktor.http.auth.HttpAuthHeader
+import kotlinx.coroutines.flow.first
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+internal class AuthenticationProvider @Inject constructor(
+    private val tokenProvider: TokenProvider,
+) : AuthProvider {
+    override val sendWithoutRequest: Boolean
+        get() = true
+    
+    override suspend fun addRequestHeaders(
+        request: HttpRequestBuilder,
+        authHeader: HttpAuthHeader?,
+    ) {
+        val accessToken = tokenProvider.accessTokenFlow.first()
+        request.headers.append("Authorization", "Bearer $accessToken")
+    }
+    
+    override fun isApplicable(auth: HttpAuthHeader): Boolean {
+        return auth is HttpAuthHeader.Single && auth.authScheme.equals("bearer", ignoreCase = true)
+    }
+}
