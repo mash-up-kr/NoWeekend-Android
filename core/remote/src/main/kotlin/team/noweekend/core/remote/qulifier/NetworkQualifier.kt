a2965dffa00930d059d9eb97b9b586d0f400e5e1
SEO-J17 <qw4643987@naver.com>
2025-05-21 23:28:30 +0900

    [#12] 네트워크 Qualifier 구현

---
+++ core/remote/src/main/kotlin/team/noweekend/core/remote/qulifier/NetworkQualifier.kt
@@ -0,0 +1,11 @@
+package team.noweekend.core.remote.qulifier
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class BasicClient
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class BaseUrl
