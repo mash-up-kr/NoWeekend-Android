6f79ca6e03c7d35910b329a8eddedb15adff9010
seojeongwoo <qw4643987@naver.com>
2025-05-22 15:48:38 +0900

    [#12] @014967 리뷰반영: 디스패처 설정 코드 삭제

--- core/remote/src/main/kotlin/team/noweekend/core/remote/base/BaseApiCall.kt
+++ core/remote/src/main/kotlin/team/noweekend/core/remote/base/BaseApiCall.kt
@@ -7,46 +7,41 @@ import io.ktor.client.request.parameter
 import io.ktor.client.request.post
 import io.ktor.client.request.put
 import io.ktor.client.request.setBody
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-
-internal suspend inline fun <reified T> safeApiCall(
-    crossinline apiCall: suspend () -> T,
-): T = withContext(Dispatchers.IO) { apiCall() }
 
 internal suspend inline fun <reified T> HttpClient.getApiCall(
     path: String,
     queries: Map<String, String>? = null,
-): T = safeApiCall {
-    this.get(path) {
+): T = this
+    .get(path) {
         queries?.forEach { (key, value) ->
             parameter(key, value)
         }
-    }.body()
-}
+    }
+    .body()
+
 
 internal suspend inline fun <reified T> HttpClient.postApiCall(
     path: String,
     body: Any? = null,
     queries: Map<String, String>? = null,
-): T = safeApiCall {
-    this.post(path) {
+): T = this
+    .post(path) {
         body?.let { setBody(body) }
         queries?.forEach { (key, value) ->
             parameter(key, value)
         }
-    }.body()
-}
+    }
+    .body()
 
 internal suspend inline fun <reified T> HttpClient.putApiCall(
     path: String,
     body: Any? = null,
     queries: Map<String, String>? = null,
-): T = safeApiCall {
-    this.put(path) {
+): T = this
+    .put(path) {
         body?.let { setBody(body) }
         queries?.forEach { (key, value) ->
             parameter(key, value)
         }
-    }.body()
-}
+    }
+    .body()
