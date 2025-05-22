c5ee09cb12cb7b75ba4ee512dd2262990f0b719e
seojeongwoo <qw4643987@naver.com>
2025-05-22 15:48:44 +0900

    [#12] lint 적용

--- core/data/src/main/java/team/noweekend/data/di/RepositoryModule.kt
+++ core/data/src/main/java/team/noweekend/data/di/RepositoryModule.kt
@@ -4,10 +4,10 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import team.noweekend.core.domain.repository.SampleRepository
 import team.noweekend.core.domain.repository.AuthRepository
-import team.noweekend.data.repository.SampleRepositoryImpl
+import team.noweekend.core.domain.repository.SampleRepository
 import team.noweekend.data.repository.AuthRepositoryImpl
+import team.noweekend.data.repository.SampleRepositoryImpl
 import javax.inject.Singleton
 
 @Module
