f11a0955a0e47be0a8940a0097c145192cd68924
SEO-J17 <qw4643987@naver.com>
2025-05-27 00:15:16 +0900

    [#12] @JaesungLee 리뷰반영 : Impl로 네이밍 변경

--- core/local/src/main/java/team/noweekend/core/local/di/DataStoreModule.kt
+++ core/local/src/main/java/team/noweekend/core/local/di/DataStoreModule.kt
@@ -18,16 +18,16 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 internal interface DataStoreModule {
-
+    
     @Singleton
     @Binds
-    fun bindLocalDataStore(localDataStore: LocalDataStoreImpl): LocalDataStore
+    fun bindLocalDataStore(localDataStoreImpl: LocalDataStoreImpl): LocalDataStore
 }
 
 @Module
 @InstallIn(SingletonComponent::class)
 internal class DataStoreProvider {
-
+    
     @Singleton
     @Provides
     fun providePreferencesDataStore(@ApplicationContext context: Context): DataStore<Preferences> {
@@ -35,7 +35,7 @@ internal class DataStoreProvider {
             produceFile = { context.preferencesDataStoreFile(PREFERENCES) }
         )
     }
-
+    
     companion object {
         private const val PREFERENCES = "user_auth_token"
     }
