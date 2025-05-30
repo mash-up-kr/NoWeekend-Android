da1b9a3d4bc1261847cda32460f9bc250365407c
JaesungLeee <viki0114@naver.com>
2025-05-30 20:00:46 +0900

    [#20] 아 얘땜에 린트 또 실패!

--- feature/sample/src/main/java/team/noweekend/feature/sample/home/screen/SampleScreen.kt
+++ feature/sample/src/main/java/team/noweekend/feature/sample/home/screen/SampleScreen.kt
@@ -12,7 +12,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import team.noweekend.feature.sample.home.mvi.SampleUiState
-import team.noweekend.feature.sample.home.mvi.SampleViewModel
 
 @Composable
 fun SampleScreen(
