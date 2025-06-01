24347739795d759b4ee7c413f1648bfb01699e57
JaesungLeee <viki0114@naver.com>
2025-06-01 23:36:37 +0900

    [#11] DesignSystem Token 정의

---
+++ core/design-system/src/main/kotlin/team/noweekend/core/design/system/foundation/color/token/NeutralColorToken.kt
@@ -0,0 +1,17 @@
+package team.noweekend.core.design.system.foundation.color.token
+
+import androidx.compose.ui.graphics.Color
+
+internal object NeutralColorToken {
+    val NeutralWhite: Color = Color(0XFFFFFFFF)
+    val NeutralGray100: Color = Color(0XFFF5F5F5)
+    val NeutralGray200: Color = Color(0XFFEEEEEE)
+    val NeutralGray300: Color = Color(0XFFDDDDDD)
+    val NeutralGray400: Color = Color(0XFFCCCCCC)
+    val NeutralGray500: Color = Color(0XFFBBBBBB)
+    val NeutralGray600: Color = Color(0XFFAAAAAA)
+    val NeutralGray700: Color = Color(0XFF999999)
+    val NeutralGray800: Color = Color(0XFF666666)
+    val NeutralGray900: Color = Color(0XFF333333)
+    val NeutralBlack: Color = Color(0XFF111111)
+}
---
+++ core/design-system/src/main/kotlin/team/noweekend/core/design/system/foundation/radius/token/BorderRadiusToken.kt
@@ -0,0 +1,17 @@
+package team.noweekend.core.design.system.foundation.radius.token
+
+import androidx.compose.foundation.shape.CornerBasedShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.unit.dp
+
+internal object BorderRadiusToken {
+    val BorderRadius0: CornerBasedShape = RoundedCornerShape(0.dp)
+    val BorderRadius50: CornerBasedShape = RoundedCornerShape(2.dp)
+    val BorderRadius100: CornerBasedShape = RoundedCornerShape(4.dp)
+    val BorderRadius200: CornerBasedShape = RoundedCornerShape(6.dp)
+    val BorderRadius300: CornerBasedShape = RoundedCornerShape(8.dp)
+    val BorderRadius400: CornerBasedShape = RoundedCornerShape(12.dp)
+    val BorderRadius500: CornerBasedShape = RoundedCornerShape(16.dp)
+    val BorderRadius600: CornerBasedShape = RoundedCornerShape(20.dp)
+    val BorderRadius700: CornerBasedShape = RoundedCornerShape(24.dp)
+}
