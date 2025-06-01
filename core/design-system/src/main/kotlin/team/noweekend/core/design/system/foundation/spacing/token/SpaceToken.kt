83a6a781cec96cb2cdb67cc823b714300cad4359
JaesungLeee <viki0114@naver.com>
2025-06-01 23:36:37 +0900

    [#11] DesignSystem Token 정의

---
+++ core/design-system/src/main/kotlin/team/noweekend/core/design/system/foundation/spacing/token/SpaceToken.kt
@@ -0,0 +1,22 @@
+package team.noweekend.core.design.system.foundation.spacing.token
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+internal object SpaceToken {
+    val Space0: Dp = 0.dp
+    val Space25: Dp = 2.dp
+    val Space50: Dp = 4.dp
+    val Space75: Dp = 6.dp
+    val Space100: Dp = 8.dp
+    val Space150: Dp = 12.dp
+    val Space175: Dp = 16.dp
+    val Space200: Dp = 20.dp
+    val Space300: Dp = 24.dp
+    val Space400: Dp = 32.dp
+    val Space500: Dp = 40.dp
+    val Space600: Dp = 48.dp
+    val Space700: Dp = 56.dp
+    val Space800: Dp = 64.dp
+    val Space900: Dp = 72.dp
+}
