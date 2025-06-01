d7885643d6a075df9b3cf0a5cfb73254a7c04740
JaesungLeee <viki0114@naver.com>
2025-06-02 00:24:40 +0900

    [#11] LocalSpacing, LocalBorderRadius 추가

--- core/design-system/src/main/kotlin/team/noweekend/core/design/system/foundation/theme/Theme.kt
+++ core/design-system/src/main/kotlin/team/noweekend/core/design/system/foundation/theme/Theme.kt
@@ -6,17 +6,47 @@ import androidx.compose.material3.LocalRippleConfiguration
 import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import team.noweekend.core.design.system.foundation.color.ColorTheme
+import team.noweekend.core.design.system.foundation.color.LocalColor
+import team.noweekend.core.design.system.foundation.radius.BorderRadius
+import team.noweekend.core.design.system.foundation.radius.LocalBorderRadius
+import team.noweekend.core.design.system.foundation.spacing.LocalSpacing
+import team.noweekend.core.design.system.foundation.spacing.Spacing
 import team.noweekend.core.design.system.foundation.theme.ripple.NWKRippleConfiguration
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NWKTheme(
+    color: ColorTheme = NWKTheme.color,
+    spacing: Spacing = NWKTheme.spacing,
+    borderRadius: BorderRadius = NWKTheme.radius,
     content: @Composable () -> Unit,
 ) {
     CompositionLocalProvider(
+        LocalColor provides color,
+        LocalSpacing provides spacing,
+        LocalBorderRadius provides borderRadius,
         LocalIndication provides ripple(),
         LocalRippleConfiguration provides NWKRippleConfiguration,
     ) {
         content()
     }
+}
+
+object NWKTheme {
+    val color: ColorTheme
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalColor.current
+
+    val spacing: Spacing
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalSpacing.current
+
+    val radius: BorderRadius
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalBorderRadius.current
 }
