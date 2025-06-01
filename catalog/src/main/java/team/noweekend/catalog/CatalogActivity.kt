9c26e87509378a1ee21265f67351b3ba2dde14c5
JaesungLeee <viki0114@naver.com>
2025-06-01 17:22:15 +0900

    [#11] CatalogActivity 네이밍 수정

--- catalog/src/main/java/team/noweekend/catalog/CatalogActivity.kt
+++ catalog/src/main/java/team/noweekend/catalog/CatalogActivity.kt
@@ -4,15 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import team.noweekend.core.design.system.foundation.NoWeekendTheme
+import team.noweekend.core.design.system.foundation.theme.NoWeekendTheme
 
-class MainActivity : ComponentActivity() {
+class CatalogActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             NoWeekendTheme {
-
             }
         }
     }
