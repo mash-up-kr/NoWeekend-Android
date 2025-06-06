b19479deae605c79c78782687440de281d0b47e3
JaesungLeee <viki0114@naver.com>
2025-06-06 22:02:14 +0900

    [#11] 기본 Button 컴포넌트 추가

---
+++ core/design-system/src/main/kotlin/team/noweekend/core/design/system/core/component/button/defaults/Button.kt
@@ -0,0 +1,58 @@
+package team.noweekend.core.design.system.core.component.button.defaults
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CornerBasedShape
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.role
+import androidx.compose.ui.semantics.semantics
+
+@Composable
+internal fun Button(
+    onClick: () -> Unit,
+    colors: NWKButtonColors,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    shape: CornerBasedShape = NWKButtonDefaults.shape,
+    border: BorderStroke? = null,
+    content: @Composable RowScope.() -> Unit,
+) {
+    val contentColor: Color by colors.contentColor(enabled)
+    val backgroundColor: Color by colors.backgroundColor(enabled)
+
+    Surface(
+        modifier = modifier
+            .semantics { role = Role.Button }
+            .clickable(
+                enabled = enabled,
+                onClick = onClick,
+            ),
+        shape = shape,
+        color = backgroundColor,
+        contentColor = contentColor,
+        border = border,
+    ) {
+        Row(
+            modifier = Modifier
+                .defaultMinSize(
+                    minWidth = NWKButtonDefaults.MinWidth,
+                    minHeight = NWKButtonDefaults.MinHeight,
+                )
+                .padding(NWKButtonDefaults.ContentPadding),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            content = content,
+        )
+    }
+}
