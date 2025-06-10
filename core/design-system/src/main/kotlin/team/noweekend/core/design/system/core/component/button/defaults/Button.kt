602f6d0abaf309d987010062dffac8e765571b07
JaesungLeee <viki0114@naver.com>
2025-06-10 11:50:24 +0900

    [#11] Button Ripple Clip 추가

--- core/design-system/src/main/kotlin/team/noweekend/core/design/system/core/component/button/defaults/Button.kt
+++ core/design-system/src/main/kotlin/team/noweekend/core/design/system/core/component/button/defaults/Button.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.semantics.role
@@ -34,6 +35,7 @@ internal fun Button(
     Surface(
         modifier = modifier
             .semantics { role = Role.Button }
+            .clip(shape)
             .clickable(
                 enabled = enabled,
                 onClick = onClick,
