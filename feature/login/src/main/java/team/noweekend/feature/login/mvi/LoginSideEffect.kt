b3b68355cd49414d1b25a971e17aa02d4aa953c2
seojeongwoo <qw4643987@naver.com>
2025-06-13 17:46:01 +0900

    [#24] 토스트 관련 SideEffect추가

--- feature/login/src/main/java/team/noweekend/feature/login/mvi/LoginSideEffect.kt
+++ feature/login/src/main/java/team/noweekend/feature/login/mvi/LoginSideEffect.kt
@@ -1,9 +1,14 @@
 package team.noweekend.feature.login.mvi
 
+import android.content.IntentSender
 import team.noweekend.core.common.android.mvi.SideEffect
 
 sealed interface LoginSideEffect : SideEffect {
     data object NavigateToOnboarding : LoginSideEffect
     data object NavigateToGoogleSignUp : LoginSideEffect
     data object ShowGoogleLoginErrorToast : LoginSideEffect
+    data object ShowGoogleLoginSuccessToast : LoginSideEffect
+    data object ShowCancelGoogleAuthToast : LoginSideEffect
+    data object ShowErrorToast : LoginSideEffect
+    data class NavigateToGoogleAuth(val intentSender: IntentSender) : LoginSideEffect
 }
