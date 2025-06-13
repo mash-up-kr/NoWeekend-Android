f1844baf42616d415cd8d527103498a15bef9a5a
JaesungLeee <viki0114@naver.com>
2025-06-13 17:36:04 +0900

    [#25] NWKNavigationBarItem 추가

---
+++ core/design-system/src/main/kotlin/team/noweekend/core/design/system/core/component/tabbar/item/NWKNavigationBarItem.kt
@@ -0,0 +1,17 @@
+package team.noweekend.core.design.system.core.component.tabbar.item
+
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemColors
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import team.noweekend.core.design.system.core.component.tabbar.item.defaults.NWKNavigationBarItemColors
+import team.noweekend.core.design.system.core.component.tabbar.item.defaults.NWKNavigationBarItemDefaults
+
+@Composable
+fun RowScope.NWKNavigationBarItem(
+    colors: NWKNavigationBarItemColors,
+    modifier: Modifier = Modifier,
+) {
+}
