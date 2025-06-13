c4769fda5d8e1a199e791a68c95b391adf0dde64
JaesungLeee <viki0114@naver.com>
2025-06-13 17:34:19 +0900

    [#25] NoWeekendActivity 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    alias(libs.plugins.team.noweekend.android.application)
+    alias(libs.plugins.team.noweekend.android.application.compose)
     alias(libs.plugins.team.noweekend.hilt)
+    alias(libs.plugins.kotlinx.serialization)
 }
 
 android {
@@ -25,5 +26,19 @@ dependencies {
     implementation(project(":feature:home"))
     implementation(project(":feature:sample"))
 
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.navigation.compose)
     implementation(libs.hilt.navigation.compose)
+    implementation(libs.bundles.kotlinx.coroutine)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.kotlinx.collections.immutable)
+
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
 }
