dc93a4e61fd46050a66f1ca987cc6927b0eefa2a
seojeongwoo <qw4643987@naver.com>
2025-06-13 17:46:18 +0900

    [#24] sideEffect 람다 추가

--- feature/login/src/main/java/team/noweekend/feature/login/mvi/LoginSideEffectHandler.kt
+++ feature/login/src/main/java/team/noweekend/feature/login/mvi/LoginSideEffectHandler.kt
@@ -1,21 +1,29 @@
 package team.noweekend.feature.login.mvi
 
+import android.content.IntentSender
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import team.noweekend.core.common.android.mvi.SideEffect
 import team.noweekend.core.common.android.mvi.SideEffectHandler
 
 @Composable
 internal fun rememberLoginSideEffectHandler(
     navigateToOnboarding: () -> Unit,
     navigateToGoogleSignUp: () -> Unit,
-    showGoogleSignUpErrorToast: () -> Unit
+    showGoogleSignUpErrorToast: () -> Unit,
+    showGoogleLoginSuccessToast: () -> Unit,
+    showCancelGoogleAuthToast: () -> Unit,
+    showErrorToast: () -> Unit,
+    navigateToGoogleAuth: (IntentSender) -> Unit,
 ): LoginSideEffectHandler {
     return remember {
         LoginSideEffectHandler(
             navigateToOnboarding = navigateToOnboarding,
             navigateToGoogleSignUp = navigateToGoogleSignUp,
-            showGoogleSignUpErrorToast = showGoogleSignUpErrorToast
+            showGoogleSignUpErrorToast = showGoogleSignUpErrorToast,
+            navigateToGoogleAuth = navigateToGoogleAuth,
+            showGoogleLoginSuccessToast = showGoogleLoginSuccessToast,
+            showCancelGoogleAuthToast = showCancelGoogleAuthToast,
+            showErrorToast = showErrorToast,
         )
     }
 }
@@ -23,13 +31,21 @@ internal fun rememberLoginSideEffectHandler(
 internal class LoginSideEffectHandler(
     private val navigateToOnboarding: () -> Unit,
     private val navigateToGoogleSignUp: () -> Unit,
-    private val showGoogleSignUpErrorToast: () -> Unit
-) : SideEffectHandler<SideEffect> {
-    override fun handleSideEffect(sideEffect: SideEffect) {
+    private val showGoogleSignUpErrorToast: () -> Unit,
+    private val showCancelGoogleAuthToast: () -> Unit,
+    private val showErrorToast: () -> Unit,
+    private val showGoogleLoginSuccessToast: () -> Unit,
+    private val navigateToGoogleAuth: (IntentSender) -> Unit,
+) : SideEffectHandler<LoginSideEffect> {
+    override fun handleSideEffect(sideEffect: LoginSideEffect) {
         when (sideEffect) {
             is LoginSideEffect.NavigateToOnboarding -> navigateToOnboarding()
             is LoginSideEffect.NavigateToGoogleSignUp -> navigateToGoogleSignUp()
             is LoginSideEffect.ShowGoogleLoginErrorToast -> showGoogleSignUpErrorToast()
+            is LoginSideEffect.NavigateToGoogleAuth -> navigateToGoogleAuth(sideEffect.intentSender)
+            is LoginSideEffect.ShowCancelGoogleAuthToast -> showCancelGoogleAuthToast()
+            is LoginSideEffect.ShowErrorToast -> showErrorToast()
+            is LoginSideEffect.ShowGoogleLoginSuccessToast -> showGoogleLoginSuccessToast()
         }
     }
 }
