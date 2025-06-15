1f95d847fe37869d8dc28f31432568aaa4cd5284
JaesungLeee <viki0114@naver.com>
2025-06-15 18:40:44 +0900

    [#25] Resource 추가

---
+++ core/resource/src/main/kotlin/team/noweekend/core/resource/NWKDrawableResource.kt
@@ -0,0 +1,10 @@
+package team.noweekend.core.resource
+
+object NWKDrawableResource {
+    val CalendarOff: Int = R.drawable.icn_calendar_off
+    val CalendarOn: Int = R.drawable.icn_calendar_on
+    val HomeOff: Int = R.drawable.icn_home_off
+    val HomeOn: Int = R.drawable.icn_home_on
+    val PersonOff: Int = R.drawable.icn_person_off
+    val PersonOn: Int = R.drawable.icn_person_on
+}
---
+++ core/resource/src/main/kotlin/team/noweekend/core/resource/NWKStringResource.kt
@@ -0,0 +1,7 @@
+package team.noweekend.core.resource
+
+object NWKStringResource {
+    val LabelHome: Int = R.string.label_home
+    val LabelCalendar: Int = R.string.label_calendar
+    val LabelProfile: Int = R.string.label_profile
+}
