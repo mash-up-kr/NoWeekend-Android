5680391cb518f35fa6c3d7fbc324faa8717420f4
JaesungLeee <viki0114@naver.com>
2025-06-19 00:09:00 +0900

    [#25] :app 모듈 불필요 의존성 제거

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.team.noweekend.android.application.compose)
     alias(libs.plugins.team.noweekend.hilt)
-    alias(libs.plugins.kotlinx.serialization)
 }
 
 android {
@@ -29,19 +28,5 @@ dependencies {
     implementation(project(":feature:calendar"))
     implementation(project(":feature:profile"))
 
-    implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.navigation.compose)
     implementation(libs.hilt.navigation.compose)
-    implementation(libs.bundles.kotlinx.coroutine)
-    implementation(libs.kotlinx.serialization.json)
-    implementation(libs.kotlinx.collections.immutable)
-
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
 }
