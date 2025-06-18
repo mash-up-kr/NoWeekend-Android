c32a61ed123852da2cf58d5e97c91f342b12011c
JaesungLeee <viki0114@naver.com>
2025-06-19 00:20:29 +0900

    [#37] HomeRoute 추가

---
+++ feature/home/src/main/java/team/noweekend/feature/home/navigation/HomeNavigation.kt
@@ -0,0 +1,12 @@
+package team.noweekend.feature.home.navigation
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import team.noweekend.core.navigator.model.Home
+import team.noweekend.feature.home.screen.HomeRoute
+
+fun NavGraphBuilder.homeNavGraph() {
+    composable<Home> {
+        HomeRoute()
+    }
+}
--- feature/main/src/main/kotlin/team/noweekend/feature/main/MainNavHost.kt
+++ feature/main/src/main/kotlin/team/noweekend/feature/main/MainNavHost.kt
@@ -1,14 +1,10 @@
 package team.noweekend.feature.main
 
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import team.noweekend.core.navigator.model.DestinationRoute
-import team.noweekend.core.navigator.model.Home
 import team.noweekend.feature.calendar.navigation.calendarNavGraph
+import team.noweekend.feature.home.navigation.homeNavGraph
 import team.noweekend.feature.main.navigation.MainNavigator
 import team.noweekend.feature.profile.navigation.profileNavGraph
 
@@ -25,11 +21,5 @@ internal fun MainNavHost(
         homeNavGraph()
         calendarNavGraph()
         profileNavGraph()
-    }
-}
-
-internal fun NavGraphBuilder.homeNavGraph() {
-    composable<Home> {
-        Text("AFAF")
     }
 }
