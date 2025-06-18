18e48c52621b652256adbb238725f1e4d4a97d1f
JaesungLeee <viki0114@naver.com>
2025-06-19 00:51:03 +0900

    [#37] NWKLongCard 추가

---
+++ core/design-system/src/main/kotlin/team/noweekend/core/design/system/core/component/card/NWKLongCard.kt
@@ -0,0 +1,62 @@
+package team.noweekend.core.design.system.core.component.card
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import team.noweekend.core.design.system.foundation.theme.NWKTheme
+
+@Composable
+fun NWKLongCard(
+    onCardClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    content: @Composable RowScope.() -> Unit,
+) {
+    Row(
+        modifier = modifier
+            .border(
+                width = 1.dp,
+                color = NWKTheme.color.Semantic.Border.border01,
+                shape = NWKTheme.radius.borderRadius500,
+            )
+            .clip(NWKTheme.radius.borderRadius500)
+            .clickable(
+                enabled = enabled,
+                onClick = onCardClick,
+            )
+            .background(
+                color = NWKTheme.color.Semantic.Background.alternative01,
+                shape = NWKTheme.radius.borderRadius500,
+            )
+            .padding(
+                horizontal = NWKTheme.spacing.space175,
+                vertical = NWKTheme.spacing.space300,
+            ),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        content = content,
+    )
+}
+
+@Preview
+@Composable
+private fun LongCardPreview() {
+    NWKTheme {
+        NWKLongCard(
+            onCardClick = {},
+            modifier = Modifier.fillMaxWidth(),
+            content = {},
+        )
+    }
+}
---
+++ feature/home/src/main/java/team/noweekend/feature/home/component/VacationRecommendCard.kt
@@ -0,0 +1,87 @@
+package team.noweekend.feature.home.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import team.noweekend.core.design.system.core.component.card.NWKLongCard
+import team.noweekend.core.design.system.foundation.theme.NWKTheme
+import team.noweekend.core.resource.NWKDrawableResource
+
+@Composable
+internal fun VacationRecommendCard(
+    onCardClick: () -> Unit,
+    imageResId: Int,
+    modifier: Modifier = Modifier,
+    iconResId: Int = NWKDrawableResource.Plus,
+) {
+    NWKLongCard(
+        modifier = modifier,
+        onCardClick = onCardClick,
+        content = {
+            VacationRecommendCardContent(
+                imageResId = imageResId,
+                iconResId = iconResId,
+            )
+        },
+    )
+}
+
+
+@Composable
+private fun RowScope.VacationRecommendCardContent(
+    imageResId: Int,
+    iconResId: Int,
+    modifier: Modifier = Modifier,
+) {
+    Image(
+        modifier = Modifier.size(62.dp),
+        painter = painterResource(id = imageResId),
+        contentDescription = null,
+    )
+    Column(
+        modifier = Modifier.weight(1f),
+        verticalArrangement = Arrangement.spacedBy(4.dp),
+    ) {
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = "Max 1line",
+            style = NWKTheme.typography.subTitle1.copy(
+                color = NWKTheme.color.Semantic.Text.body,
+            ),
+        )
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = "0/00(월) ~ 0/00(월)",
+            style = NWKTheme.typography.heading6.copy(
+                color = NWKTheme.color.Semantic.Text.neutral,
+            ),
+        )
+    }
+    Icon(
+        modifier = Modifier.size(24.dp),
+        painter = painterResource(iconResId),
+        tint = NWKTheme.color.Neutral.neutralGray700,
+        contentDescription = null,
+    )
+}
+
+@Preview
+@Composable
+private fun VacationRecommendCardPreview() {
+    NWKTheme {
+        VacationRecommendCard(
+            onCardClick = {},
+            imageResId = NWKDrawableResource.HomeOn,
+        )
+    }
+}
