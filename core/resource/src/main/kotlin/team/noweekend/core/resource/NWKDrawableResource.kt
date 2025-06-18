4e16c41696a7171d9b6f75ee55e97abadefafe1d
JaesungLeee <viki0114@naver.com>
2025-06-19 01:03:08 +0900

    [#37] 이미지 DrawableResource 추가

--- core/resource/src/main/kotlin/team/noweekend/core/resource/NWKDrawableResource.kt
+++ core/resource/src/main/kotlin/team/noweekend/core/resource/NWKDrawableResource.kt
@@ -1,6 +1,7 @@
 package team.noweekend.core.resource
 
 object NWKDrawableResource {
+    /* Icon */
     val CalendarOff: Int = R.drawable.icn_calendar_off
     val CalendarOn: Int = R.drawable.icn_calendar_on
     val HomeOff: Int = R.drawable.icn_home_off
@@ -8,5 +9,15 @@ object NWKDrawableResource {
     val PersonOff: Int = R.drawable.icn_person_off
     val PersonOn: Int = R.drawable.icn_person_on
     val Plus: Int = R.drawable.icn_plus
+    val Filter: Int = R.drawable.icn_filter
+
+    /* Image */
+    val Cake: Int = R.drawable.img_cake
+    val Home: Int = R.drawable.img_home
+    val Korea: Int = R.drawable.img_korea
+    val Sandwich: Int = R.drawable.img_sandwich
+    val Star: Int = R.drawable.img_star
+    val Train: Int = R.drawable.img_train
+    val Trip: Int = R.drawable.img_trip
     val MainToaster: Int = R.drawable.img_main_toaster
 }
