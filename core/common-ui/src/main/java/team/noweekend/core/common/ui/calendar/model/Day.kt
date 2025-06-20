523d44a54e871918a891ebf4a096dce4e7f1b469
Boris-xyz <boris@xyzcorp.io>
2025-06-20 10:31:17 +0900

    [#10] kotlinx DayOfWeek으로 변경

--- core/common-ui/src/main/java/team/noweekend/core/common/ui/calendar/model/Day.kt
+++ core/common-ui/src/main/java/team/noweekend/core/common/ui/calendar/model/Day.kt
@@ -1,68 +1,58 @@
 package team.noweekend.core.common.ui.calendar.model
 
 import androidx.annotation.StringRes
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
 import androidx.compose.ui.graphics.Color
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
+import kotlinx.datetime.DayOfWeek
+import team.noweekend.core.design.system.foundation.theme.NWKTheme
 import team.noweekend.core.resource.R
 
-enum class DayId {
-    SUNDAY,
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY,
-}
-
+@Stable
 sealed interface Day {
-    val dayId: DayId
+    val dayId: DayOfWeek
 
-    @get:StringRes
     val id: Int
+        @StringRes get
+
     val color: Color
+        @Composable get () = NWKTheme.color.Neutral.neutralGray600
 
     data class Sunday(
-        override val dayId: DayId = DayId.SUNDAY,
+        override val dayId: DayOfWeek = DayOfWeek.SUNDAY,
         override val id: Int = R.string.sunday,
-        override val color: Color = Color.Black,
     ) : Day
 
     data class Monday(
-        override val dayId: DayId = DayId.MONDAY,
+        override val dayId: DayOfWeek = DayOfWeek.MONDAY,
         override val id: Int = R.string.monday,
-        override val color: Color = Color.Black,
     ) : Day
 
     data class TuesDay(
-        override val dayId: DayId = DayId.TUESDAY,
+        override val dayId: DayOfWeek = DayOfWeek.TUESDAY,
         override val id: Int = R.string.tuesday,
-        override val color: Color = Color.Black,
     ) : Day
 
     data class Wednesday(
-        override val dayId: DayId = DayId.WEDNESDAY,
+        override val dayId: DayOfWeek = DayOfWeek.WEDNESDAY,
         override val id: Int = R.string.wednesday,
-        override val color: Color = Color.Black,
     ) : Day
 
     data class Thursday(
-        override val dayId: DayId = DayId.THURSDAY,
+        override val dayId: DayOfWeek = DayOfWeek.THURSDAY,
         override val id: Int = R.string.thursday,
-        override val color: Color = Color.Black,
     ) : Day
 
     data class Friday(
-        override val dayId: DayId = DayId.FRIDAY,
+        override val dayId: DayOfWeek = DayOfWeek.FRIDAY,
         override val id: Int = R.string.friday,
-        override val color: Color = Color.Black,
     ) : Day
 
     data class Saturday(
-        override val dayId: DayId = DayId.SATURDAY,
+        override val dayId: DayOfWeek = DayOfWeek.SATURDAY,
         override val id: Int = R.string.saturday,
-        override val color: Color = Color.Black,
     ) : Day
 
     companion object {
