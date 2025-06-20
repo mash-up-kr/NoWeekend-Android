7382c14642331f8c03c9fc9f92dfde06bd8905e9
JaesungLeee <viki0114@naver.com>
2025-06-21 00:07:54 +0900

    [#42] :core:common-ui 모듈 의존성 추가

--- core/common-ui/build.gradle.kts
+++ core/common-ui/build.gradle.kts
@@ -10,6 +10,8 @@ android {
 dependencies {
     implementation(project(":core:common-android"))
     implementation(project(":core:common-kotlin"))
+    implementation(project(":core:design-system"))
+    implementation(project(":core:resource"))
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
