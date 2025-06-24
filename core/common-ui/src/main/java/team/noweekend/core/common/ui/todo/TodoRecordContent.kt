243154aa5c5e074c1e15bab990f19961138841b5
Boris-xyz <boris@xyzcorp.io>
2025-06-24 11:20:56 +0900

    [#10] 완료시 알파 추가

--- core/common-ui/src/main/java/team/noweekend/core/common/ui/todo/TodoRecordContent.kt
+++ core/common-ui/src/main/java/team/noweekend/core/common/ui/todo/TodoRecordContent.kt
@@ -13,7 +13,9 @@ import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import team.noweekend.core.common.ui.todo.model.Todo
@@ -26,7 +28,15 @@ fun TodoRecordContent(
     modifier: Modifier = Modifier,
 ) {
     Column(
-        modifier = modifier.fillMaxWidth(),
+        modifier = modifier
+            .fillMaxWidth()
+            .graphicsLayer {
+                if (todo.isDone) {
+                    alpha = 0.5f
+                } else {
+                    alpha = 1f
+                }
+            },
         verticalArrangement = Arrangement.spacedBy(4.dp),
     ) {
         Text(
@@ -34,6 +44,8 @@ fun TodoRecordContent(
             text = todo.title,
             style = NWKTheme.typography.body2,
             color = NWKTheme.color.Semantic.Text.neutral,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
         )
         Row(
             modifier = Modifier
@@ -62,7 +74,6 @@ fun TodoRecordContent(
         }
     }
 }
-
 
 @Preview(showBackground = true)
 @Composable
