5e5f0646779b816f19b9724dafab7f265e8c4168
JaesungLeee <viki0114@naver.com>
2025-06-24 11:01:46 +0900

    ktlint check

--- feature/home/src/main/java/team/noweekend/feature/home/component/recommend/personal/carousel/PersonalRecommendCarousel.kt
+++ feature/home/src/main/java/team/noweekend/feature/home/component/recommend/personal/carousel/PersonalRecommendCarousel.kt
@@ -8,9 +8,6 @@ import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PageSize
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
