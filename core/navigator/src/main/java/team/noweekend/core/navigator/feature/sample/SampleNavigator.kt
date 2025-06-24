03197de35f43d59acc135e21fd9c560169815988
JaesungLeee <viki0114@naver.com>
2025-06-24 11:13:16 +0900

    [#45] 미사용 코드 제거 (sample 모듈 제거)

--- core/navigator/src/main/java/team/noweekend/core/navigator/feature/sample/SampleNavigator.kt
+++
@@ -1,5 +0,0 @@
-package team.noweekend.core.navigator.feature.sample
-
-import team.noweekend.core.navigator.base.Navigator
-
-interface SampleNavigator : Navigator
--- feature/sample/src/main/java/team/noweekend/feature/sample/detail/mvi/SampleDetailSideEffect.kt
+++
@@ -1,7 +0,0 @@
-package team.noweekend.feature.sample.detail.mvi
-
-import team.noweekend.core.common.android.mvi.SideEffect
-
-sealed interface SampleDetailSideEffect : SideEffect {
-    data object NavigateToHistoryBack : SampleDetailSideEffect
-}
