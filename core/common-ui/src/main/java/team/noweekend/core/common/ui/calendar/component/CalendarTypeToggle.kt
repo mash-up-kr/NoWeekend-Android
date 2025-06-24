266c8f93250c5a32c9ed400133ae2dc79c4bb7cd
Boris-xyz <boris@xyzcorp.io>
2025-06-24 12:17:05 +0900

    [#10] ktlint 적용

--- core/common-ui/src/main/java/team/noweekend/core/common/ui/calendar/component/CalendarTypeToggle.kt
+++ core/common-ui/src/main/java/team/noweekend/core/common/ui/calendar/component/CalendarTypeToggle.kt
@@ -31,7 +31,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
