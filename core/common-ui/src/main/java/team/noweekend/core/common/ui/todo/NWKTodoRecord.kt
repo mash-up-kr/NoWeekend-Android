9c1374fe341eb7ef01922ff803db6217688b396c
Boris-xyz <boris@xyzcorp.io>
2025-06-24 11:50:30 +0900

    [#10] ktlint 적용

--- core/common-ui/src/main/java/team/noweekend/core/common/ui/todo/NWKTodoRecord.kt
+++ core/common-ui/src/main/java/team/noweekend/core/common/ui/todo/NWKTodoRecord.kt
@@ -4,15 +4,12 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import team.noweekend.core.common.ui.todo.model.Todo
-import team.noweekend.core.common.ui.todo.model.TodoType
 import team.noweekend.core.common.ui.todo.preview.PreviewTodoRecordParameterProvider
 import team.noweekend.core.design.system.core.component.checkbox.NWKCheckBox
 import team.noweekend.core.design.system.foundation.theme.NWKTheme
@@ -53,4 +50,3 @@ private fun PreviewNWKTodoRecord(
         )
     }
 }
-
--- core/common-ui/src/main/java/team/noweekend/core/common/ui/todo/preview/PreviewTodoRecordParameterProvider.kt
+++ core/common-ui/src/main/java/team/noweekend/core/common/ui/todo/preview/PreviewTodoRecordParameterProvider.kt
@@ -13,25 +13,25 @@ internal class PreviewTodoRecordParameterProvider : PreviewParameterProvider<Tod
                 title = "출근하기",
                 description = "오전 9시 30분까지 출근하기",
                 isDone = true,
-                todoType = TodoType.Company()
+                todoType = TodoType.Company(),
             ),
             Todo(
                 title = "친구 약속",
                 description = "밥먹기",
                 isDone = true,
-                todoType = TodoType.Personal()
+                todoType = TodoType.Personal(),
             ),
             Todo(
                 title = "여행",
                 description = "제주도 여행",
                 isDone = false,
-                todoType = TodoType.AnnualLeave()
+                todoType = TodoType.AnnualLeave(),
             ),
             Todo(
                 title = "집콕",
                 description = "집에서 쉬기",
                 isDone = false,
-                todoType = TodoType.Etc()
-            )
+                todoType = TodoType.Etc(),
+            ),
         )
 }
