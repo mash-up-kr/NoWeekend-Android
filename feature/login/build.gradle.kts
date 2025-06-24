4935c6dd0928a1e7f57f5c1a61666ff7cb257a80
seojeongwoo <qw4643987@naver.com>
2025-06-24 16:22:35 +0900

    [#24] @JaesungLeee 리뷰 반영 : 컨벤셩 플러그인에buildConfigField 추가

--- feature/login/build.gradle.kts
+++ feature/login/build.gradle.kts
@@ -1,19 +1,9 @@
-import java.util.Properties
-
-val localProperties = Properties()
-val localPropertiesFile = rootProject.file("local.properties")
-if (localPropertiesFile.exists()) {
-    localProperties.load(localPropertiesFile.inputStream())
-}
-
 plugins {
     alias(libs.plugins.team.noweekend.android.feature)
 }
+
 android {
     namespace = "team.noweekend.feature.login"
-    defaultConfig {
-        buildConfigField("String", "GOOGLE_CLIENT_ID", "\"${localProperties["GOOGLE_CLIENT_ID"]}\"")
-    }
 }
 
 dependencies {
