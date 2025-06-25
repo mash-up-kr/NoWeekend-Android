f6d9f70550b4b6ed3598a184684c5737cfca596c
JaesungLeee <viki0114@naver.com>
2025-06-25 22:54:48 +0900

    ktlint check

--- feature/home/src/main/java/team/noweekend/feature/home/component/holiday/HolidayRecommendComponent.kt
+++ feature/home/src/main/java/team/noweekend/feature/home/component/holiday/HolidayRecommendComponent.kt
@@ -1,10 +1,8 @@
 package team.noweekend.feature.home.component.holiday
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -12,7 +10,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import team.noweekend.core.common.android.extension.fillMaxWidthOfScreen
-import team.noweekend.core.design.system.core.component.scaffold.NWKScaffold
 import team.noweekend.core.design.system.foundation.theme.NWKTheme
 import team.noweekend.core.resource.NWKStringResource
 import team.noweekend.feature.home.component.holiday.carousel.HolidayRecommendCarousel
