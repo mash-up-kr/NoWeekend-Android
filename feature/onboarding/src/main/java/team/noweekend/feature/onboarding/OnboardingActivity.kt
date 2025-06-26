7f68fbc8feb5b79749ca73817b4ce463ddb6054a
seojeongwoo <qw4643987@naver.com>
2025-06-26 17:57:50 +0900

    [#49] 온보딩 모듈 생성

---
+++ feature/onboarding/src/main/java/team/noweekend/feature/onboarding/OnboardingActivity.kt
@@ -0,0 +1,12 @@
+package team.noweekend.feature.onboarding
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class OnboardingActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
