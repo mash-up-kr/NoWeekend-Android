d52737494aa8d65ddfd6ac8628f4b418dbce2ba1
seojeongwoo <qw4643987@naver.com>
2025-06-27 21:13:58 +0900

    [#49] 온보딩 string 추가

--- core/resource/src/main/kotlin/team/noweekend/core/resource/NWKStringResource.kt
+++ core/resource/src/main/kotlin/team/noweekend/core/resource/NWKStringResource.kt
@@ -19,4 +19,21 @@ object NWKStringResource {
 
     val Week: Int = R.string.week
     val Month: Int = R.string.month
+
+    val OnboardingNicknameTitle = R.string.onboard_title
+    val OnboardingNicknameSubTitle = R.string.onboard_subtitle
+    val OnboardingNicknameInputHint = R.string.onboard_nickname_input_hint
+    val InputDateError = R.string.input_date_error
+    val InputVacationError = R.string.input_vacation_error
+    val Day = R.string.day
+    val Time = R.string.time
+    val Next = R.string.next
+    val VacationDayTimeFormat = R.string.vacation_day_time_format
+    val VacationSeparator = R.string.vacation_separator
+    val VacationMyVacationFormat = R.string.vacation_my_vacation_format
+    val VacationInputTitle = R.string.vacation_input_title
+    val VacationInputSubTitle = R.string.vacation_input_subtitle
+    val RemainingVacationLabel = R.string.remaining_vacation_label
+    val AllVacationLabel = R.string.all_vacation_label
+    val DefaultDayNumber = R.string.default_day_number
 }
