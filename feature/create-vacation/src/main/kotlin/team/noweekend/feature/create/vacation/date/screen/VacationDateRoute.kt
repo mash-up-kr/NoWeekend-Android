af694148b5b912cd4a593fc60d0717eef7a21787
JaesungLeee <viki0114@naver.com>
2025-06-29 03:52:36 +0900

    [#60] :feature:create-vacation 모듈 생성

---
+++ feature/create-vacation/src/main/kotlin/team/noweekend/feature/create/vacation/date/screen/VacationDateRoute.kt
@@ -0,0 +1,11 @@
+package team.noweekend.feature.create.vacation.date.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+internal fun VacationDateRoute(
+    modifier: Modifier = Modifier,
+) {
+    VacationDateScreen()
+}
---
+++ feature/create-vacation/src/main/kotlin/team/noweekend/feature/create/vacation/information/screen/InformationScreen.kt
@@ -0,0 +1,11 @@
+package team.noweekend.feature.create.vacation.information.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+internal fun InformationScreen(
+    modifier: Modifier = Modifier,
+) {
+
+}
