b030ae280b7b8fca31aab5841487d6eca7f32858
Boris-xyz <boris@xyzcorp.io>
2025-07-02 23:09:36 +0900

    [#69] path에서 androidx.graphics.shapes로 의존성 넣어서 변경

--- feature/calendar/src/main/kotlin/team/noweekend/feature/calendar/component/choose/ChooseYearMonthButton.kt
+++ feature/calendar/src/main/kotlin/team/noweekend/feature/calendar/component/choose/ChooseYearMonthButton.kt
@@ -1,25 +1,23 @@
 package team.noweekend.feature.calendar.component.choose
 
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Rect
-import androidx.compose.ui.graphics.Outline
-import androidx.compose.ui.graphics.Paint
-import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.PathEffect
-import androidx.compose.ui.graphics.drawOutline
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.graphics.asComposePath
 import androidx.compose.ui.graphics.drawscope.Fill
-import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.graphics.shapes.CornerRounding
+import androidx.graphics.shapes.RoundedPolygon
+import androidx.graphics.shapes.toPath
 import team.noweekend.core.design.system.foundation.theme.NWKTheme
 import team.noweekend.core.resource.NWKStringResource.YearMonthFormat
 
@@ -43,46 +41,57 @@ fun ChooseYearMonthButton(
             color = NWKTheme.color.Neutral.black,
         )
         val triangleColor = NWKTheme.color.Semantic.Text.body
-        Canvas(
-            modifier = Modifier.size(24.dp),
-        ) {
-            val width = size.width
 
-            val left: Float = 5.5.dp.toPx()
-            val leftTop: Float = 9.dp.toPx()
-            val right: Float = 18.5.dp.toPx()
-            val bottom: Float = 18.dp.toPx()
+        Box(
+            modifier = Modifier
+                .drawWithCache {
+                    val left = 5.5.dp.toPx()
+                    val leftTop = 9.dp.toPx()
+                    val right = 18.5.dp.toPx()
+                    val bottom = 17.dp.toPx()
+                    val centerX = left + (right - left) / 2
 
-            val rect = Rect(left, leftTop, right, bottom)
+                    // FloatArray로 꼭짓점 좌표 정의
+                    val vertices = floatArrayOf(
+                        left, leftTop, // 왼쪽 꼭짓점
+                        right, leftTop, // 오른쪽 꼭짓점
+                        centerX, bottom, // 아래쪽 꼭짓점
+                    )
 
-            val path = Path().apply {
-                moveTo(left, leftTop)
-                lineTo(right, leftTop)
-                lineTo(width / 2, bottom)
-                close()
-            }
+                    val density = this.density
+                    val adjustedRadius = 1.5f * density
+                    val adjustedSmoothing = 0.6f * density
 
-            drawIntoCanvas { canvas->
-                canvas.drawOutline(
-                    outline = Outline.Generic(path),
-                    paint = Paint().apply {
-                        color = triangleColor
-                        pathEffect = PathEffect.cornerPathEffect(rect.maxDimension / 3f)
+                    val roundedPolygon = RoundedPolygon(
+                        vertices = vertices,
+                        rounding = CornerRounding(
+                            radius = adjustedRadius, // 피그마의 corner radius 1.5px
+                            smoothing = adjustedSmoothing, // 피그마의 corner smoothing 60%
+                        ),
+                    )
+                    val roundedPolygonPath = roundedPolygon.toPath().asComposePath()
+
+                    onDrawBehind {
+                        drawPath(
+                            path = roundedPolygonPath,
+                            color = triangleColor,
+                            style = Fill,
+                        )
                     }
-                )
-            }
-        }
+                }
+                .size(24.dp),
+        )
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 private fun PreviewChooseYearMonthButton() {
     NWKTheme {
         ChooseYearMonthButton(
             year = 2023,
             month = 10,
-            onClick = {}
+            onClick = {},
         )
     }
 }
