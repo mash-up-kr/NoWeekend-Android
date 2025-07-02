6505c8c68b3e6839baece622bdff88a4bbad4fd8
Boris-xyz <boris@xyzcorp.io>
2025-07-02 19:34:47 +0900

    [#67] 텍스트와 게이지 합쳐 컴포넌트 완성 및 프리뷰 구현

---
+++ feature/calendar/src/main/kotlin/team/noweekend/feature/calendar/component/degreeGauge/preview/PreviewDegreeGaugeParameterProvider.kt
@@ -0,0 +1,19 @@
+package team.noweekend.feature.calendar.component.degreeGauge.preview
+
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+
+class PreviewDegreeGaugeParameterProvider : PreviewParameterProvider<Pair<Int, Boolean>> {
+    override val values: Sequence<Pair<Int, Boolean>> = sequenceOf(
+        Pair(0, true),
+        Pair(0, false),
+        Pair(24, false),
+        Pair(25, false),
+        Pair(26, false),
+        Pair(49, false),
+        Pair(50, false),
+        Pair(51, false),
+        Pair(75, false),
+        Pair(99, false),
+        Pair(100, false),
+    )
+}
