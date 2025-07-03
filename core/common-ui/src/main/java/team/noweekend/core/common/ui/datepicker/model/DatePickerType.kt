703e80a250dc751003097ebc5b15615a0d2aa7f1
Boris-xyz <boris@xyzcorp.io>
2025-07-03 12:06:12 +0900

    [#74] custom model 제거

--- core/common-ui/src/main/java/team/noweekend/core/common/ui/datepicker/model/DatePickerType.kt
+++ core/common-ui/src/main/java/team/noweekend/core/common/ui/datepicker/model/DatePickerType.kt
@@ -1,11 +1,4 @@
 package team.noweekend.core.common.ui.datepicker.model
-
-data class WheelDate(
-    val year: Int,
-    val month: Int,
-    val day: Int,
-)
-
 enum class DatePickerType {
     YearMonth, YearMonthDay
 }
