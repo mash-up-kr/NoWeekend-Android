0eb31c426ce7da7429ed62ebf17d388d6c0739ae
JaesungLeee <viki0114@naver.com>
2025-07-03 10:44:50 +0900

    [#70] scrollable 제거 (verticalScroll)

--- feature/create-vacation/src/main/kotlin/team/noweekend/feature/create/vacation/information/screen/InformationScreen.kt
+++ feature/create-vacation/src/main/kotlin/team/noweekend/feature/create/vacation/information/screen/InformationScreen.kt
@@ -1,8 +1,6 @@
 package team.noweekend.feature.create.vacation.information.screen
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
@@ -95,7 +94,7 @@ private fun InformationScreenContent(
     Column(
         modifier = modifier
             .fillMaxSize()
-            .scrollable(state = rememberScrollState(), orientation = Orientation.Vertical),
+            .verticalScroll(state = rememberScrollState()),
     ) {
         Spacer(Modifier.size(NWKTheme.spacing.space600))
         Text(
