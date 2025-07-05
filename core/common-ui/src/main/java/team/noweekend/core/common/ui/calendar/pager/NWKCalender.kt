0292bfacf8d3e34bcd17ba9e7841e3bf334e0173
Boris-xyz <boris@xyzcorp.io>
2025-07-05 22:34:37 +0900

    [#78] 데이터 계산 로직 hoisting

--- core/common-ui/src/main/java/team/noweekend/core/common/ui/calendar/pager/NWKCalender.kt
+++ core/common-ui/src/main/java/team/noweekend/core/common/ui/calendar/pager/NWKCalender.kt
@@ -1,49 +1,32 @@
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.VerticalPager
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.State
+import androidx.compose.runtime.snapshots.SnapshotStateMap
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import team.noweekend.core.common.ui.calendar.CalendarDataProvider
-import team.noweekend.core.common.ui.calendar.CalendarDataProvider.CalendarDataProviderEvent
+import kotlinx.datetime.LocalDate
 import team.noweekend.core.common.ui.calendar.component.CalendarItem
-import team.noweekend.core.common.ui.calendar.component.CalendarTypeToggle
 import team.noweekend.core.common.ui.calendar.component.DayOfWeekBar
+import team.noweekend.core.common.ui.calendar.model.DateOfWeek
 import team.noweekend.core.common.ui.calendar.model.WeeksData
-import team.noweekend.core.common.ui.calendar.rememberCalendarDataProvider
-import team.noweekend.core.common.ui.calendar.state.CalendarPagerState
 import team.noweekend.core.common.ui.calendar.state.CalendarPagerState.CalendarMode
-import team.noweekend.core.common.ui.calendar.state.rememberCalendarPagerState
-import team.noweekend.core.design.system.foundation.theme.NWKTheme
 
 @Composable
 fun NWKCalender(
+    mode: CalendarMode,
+    weekPagerState: PagerState,
+    monthPagerState: PagerState,
+    selectedDate: State<LocalDate>,
+    calendarItemClickable: Boolean,
+    monthData: SnapshotStateMap<Int, WeeksData>,
+    weeksData: SnapshotStateMap<Int, WeeksData>,
+    onClickDateOfWeek: (DateOfWeek) -> Unit,
     modifier: Modifier = Modifier,
-    calendarPagerState: CalendarPagerState = rememberCalendarPagerState(),
-    calendarDataProvider: CalendarDataProvider = rememberCalendarDataProvider(),
+
 ) {
-    val mode by calendarPagerState.calendarMode.collectAsState()
-
-    LaunchedEffect(Unit) {
-        calendarDataProvider.calendarDataProviderEventFlow.collect { event ->
-            when (event) {
-                is CalendarDataProviderEvent.CompleteInitWeeksCalendar -> {
-                    calendarPagerState.scrollToInitialWeekPage()
-                }
-
-                is CalendarDataProviderEvent.CompleteInitMonthCalendar -> {
-                    calendarPagerState.scrollToMonthPage()
-                }
-            }
-        }
-    }
 
     Column(
         modifier = modifier,
@@ -51,60 +34,37 @@ fun NWKCalender(
         DayOfWeekBar(modifier = Modifier.fillMaxWidth(), isMondayStarted = true)
         when (mode) {
             CalendarMode.WEEK -> {
-                LaunchedEffect(Unit) {
-                    calendarDataProvider.initWeekCalendar(
-                        initPage = calendarPagerState.initialPage,
-                    )
-                }
-                LaunchedEffect(calendarPagerState.weekPagerState.currentPage) {
-                    val currentPage = calendarPagerState.weekPagerState.currentPage
-                    calendarPagerState.updateWeekCalendar(
-                        currentPage = currentPage,
-                        updatePreviousWeekPage = calendarDataProvider::updatePreviousWeeksData,
-                        updateNextWeekPage = calendarDataProvider::updateNextWeeksData,
-                    )
-                }
-
                 HorizontalPager(
-                    state = calendarPagerState.weekPagerState,
+                    state = weekPagerState,
                     modifier = Modifier.fillMaxWidth(),
                 ) { page ->
 
-                    val weekDates = calendarDataProvider.weeksData[page] ?: WeeksData.default
+                    val weekDates = weeksData[page] ?: WeeksData.default
 
                     CalendarItem(
                         dataList = weekDates,
                         calendarMode = mode,
-                        targetDate = calendarDataProvider.targetDate,
-                        onClickDateOfWeek = calendarDataProvider::updateTargetDate,
+                        calendarItemClickable = calendarItemClickable,
+                        targetDate = selectedDate,
+                        onClickDateOfWeek = onClickDateOfWeek,
                     )
                 }
             }
 
             CalendarMode.MONTH -> {
-                LaunchedEffect(Unit) {
-                    calendarDataProvider.initMonthCalendar(page = calendarPagerState.initialPage)
-                }
-
-                LaunchedEffect(calendarPagerState.monthPagerState.currentPage) {
-                    calendarPagerState.updateMonthCalendar(
-                        currentPage = calendarPagerState.monthPagerState.currentPage,
-                        updateNextMonthPage = calendarDataProvider::updateNextMonthData,
-                        updatePreviousMonthPage = calendarDataProvider::updatePreviousMonthData,
-                    )
-                }
                 VerticalPager(
-                    state = calendarPagerState.monthPagerState,
+                    state = monthPagerState,
                     modifier = Modifier.fillMaxWidth(),
                 ) { page ->
 
-                    val dataList = calendarDataProvider.monthData[page] ?: WeeksData.default
+                    val dataList = monthData[page] ?: WeeksData.default
 
                     CalendarItem(
                         dataList = dataList,
                         calendarMode = mode,
-                        targetDate = calendarDataProvider.targetDate,
-                        onClickDateOfWeek = calendarDataProvider::updateTargetDate,
+                        calendarItemClickable = calendarItemClickable,
+                        targetDate = selectedDate,
+                        onClickDateOfWeek = onClickDateOfWeek,
                     )
                 }
             }
@@ -112,46 +72,50 @@ fun NWKCalender(
     }
 }
 
-@Preview
-@Composable
-private fun PreviewCalendar() {
-    val calendarDataProvider = rememberCalendarDataProvider()
-    val calendarPagerState = rememberCalendarPagerState()
-    val calendarMode by calendarPagerState.calendarMode.collectAsState()
-
-    NWKTheme {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(color = NWKTheme.color.Neutral.white),
-        ) {
-            CalendarTypeToggle(
-                modifier = Modifier.align(Alignment.End),
-                currentCalendarMode = calendarMode,
-                onToggleStateChanged = { isMonth ->
-                    if (isMonth) {
-                        calendarPagerState.updateCalendarMode(calendarMode = CalendarMode.MONTH)
-                    } else {
-                        calendarPagerState.updateCalendarMode(calendarMode = CalendarMode.WEEK)
-                        calendarDataProvider.initWeekCalendar(initPage = calendarPagerState.initialPage)
-                        calendarPagerState.scrollToInitialWeekPage()
-                    }
-                },
-                onClickToggle = {
-                    calendarPagerState.updateCalendarMode(
-                        calendarMode = if (calendarPagerState.calendarMode.value == CalendarMode.WEEK) {
-                            CalendarMode.MONTH
-                        } else {
-                            CalendarMode.WEEK
-                        },
-                    )
-                },
-
-            )
-            NWKCalender(
-                calendarPagerState = calendarPagerState,
-                calendarDataProvider = calendarDataProvider,
-            )
-        }
-    }
-}
+// @Preview
+// @Composable
+// private fun PreviewCalendar() {
+//    val calendarDataProvider = rememberCalendarDataProvider()
+//    val calendarPagerState = rememberCalendarPagerState()
+//    val calendarMode by calendarPagerState.calendarMode.collectAsState()
+//    val coroutineScope = rememberCoroutineScope()
+//
+//    NWKTheme {
+//        Column(
+//            modifier = Modifier
+//                .fillMaxSize()
+//                .background(color = NWKTheme.color.Neutral.white),
+//        ) {
+//            CalendarTypeToggle(
+//                modifier = Modifier.align(Alignment.End),
+//                currentCalendarMode = calendarMode,
+//                onToggleStateChanged = { isMonth ->
+//                    if (isMonth) {
+//                        calendarPagerState.updateCalendarMode(calendarMode = CalendarMode.MONTH)
+//                    } else {
+//                        calendarPagerState.updateCalendarMode(calendarMode = CalendarMode.WEEK)
+//                        coroutineScope.launch {
+//                            calendarDataProvider.initWeekCalendar(initPage = calendarPagerState.initialPage)
+//                        }
+//
+//                        calendarPagerState.scrollToInitialWeekPage()
+//                    }
+//                },
+//                onClickToggle = {
+//                    calendarPagerState.updateCalendarMode(
+//                        calendarMode = if (calendarPagerState.calendarMode.value == CalendarMode.WEEK) {
+//                            CalendarMode.MONTH
+//                        } else {
+//                            CalendarMode.WEEK
+//                        },
+//                    )
+//                },
+//
+//                )
+//            NWKCalender(
+//                calendarPagerState = calendarPagerState,
+//                calendarDataProvider = calendarDataProvider,
+//            )
+//        }
+//    }
+// }
