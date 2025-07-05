fd8cdd5c32408788d1eb7f6eaa342e9556066fe4
Boris-xyz <boris@xyzcorp.io>
2025-07-06 00:33:57 +0900

    [#82] string resource 추가

--- core/resource/src/main/kotlin/team/noweekend/core/resource/NWKStringResource.kt
+++ core/resource/src/main/kotlin/team/noweekend/core/resource/NWKStringResource.kt
@@ -62,4 +62,6 @@ object NWKStringResource {
     val DegreeCardTitle: Int= R.string.degree_card_title
     val DegreeCardAnnualLeaveTitle: Int = R.string.degree_card_annual_leave_title
 
+    val MonthChooseButtonTitle : Int = R.string.month_choose_button_title
+
 }
