f2bcf2104994f93e3868b56664077721c5ab342a
JaesungLeee <viki0114@naver.com>
2025-07-07 19:41:54 +0900

    [#70] 로딩 컴포넌트 추가

---
+++ feature/create-vacation/src/main/kotlin/team/noweekend/feature/create/vacation/recommend/component/loading/LoadingComponent.kt
@@ -0,0 +1,36 @@
+package team.noweekend.feature.create.vacation.recommend.component.loading
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import team.noweekend.core.design.system.foundation.theme.NWKTheme
+import team.noweekend.feature.create.vacation.recommend.component.lottie.LoadingAnimation
+
+@Composable
+internal fun BoxScope.LoadingComponent(
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier.align(Alignment.Center),
+        verticalArrangement = Arrangement.spacedBy(NWKTheme.spacing.space175),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = "휴가를 굽고 있어요",
+            style = NWKTheme.typography.heading4.copy(
+                fontWeight = FontWeight.W700,
+                color = NWKTheme.color.Semantic.Text.neutral,
+            ),
+        )
+        LoadingAnimation(
+            modifier = Modifier.size(width = 52.dp, height = 24.dp),
+        )
+    }
+}
