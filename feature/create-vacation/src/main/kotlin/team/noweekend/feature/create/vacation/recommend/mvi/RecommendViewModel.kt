17d4ed48d19e4e0340b197bc1b6ad6968531d997
JaesungLeee <viki0114@naver.com>
2025-07-08 10:47:36 +0900

    [#70] UIModel 추가

--- feature/create-vacation/src/main/kotlin/team/noweekend/feature/create/vacation/recommend/mvi/RecommendViewModel.kt
+++ feature/create-vacation/src/main/kotlin/team/noweekend/feature/create/vacation/recommend/mvi/RecommendViewModel.kt
@@ -1,10 +1,11 @@
 package team.noweekend.feature.create.vacation.recommend.mvi
 
-import androidx.compose.runtime.Composable
 import androidx.lifecycle.SavedStateHandle
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
 import team.noweekend.core.common.android.base.MVIViewModel
+import team.noweekend.feature.create.vacation.recommend.model.RecommendVacationType
+import team.noweekend.feature.create.vacation.recommend.model.RecommendedVacationUiModel
 import javax.inject.Inject
 
 @HiltViewModel
@@ -16,6 +17,7 @@ class RecommendViewModel @Inject constructor(
     init {
         loading()
     }
+
     override fun createInitialState(savedStateHandle: SavedStateHandle): RecommendUiState {
         return RecommendUiState.INITIAL_STATE
     }
@@ -37,6 +39,14 @@ class RecommendViewModel @Inject constructor(
     private fun loading() = execute {
         reduce { copy(isLoading = true) }
         delay(4000L)
+        reduce {
+            copy(
+                recommendedVacation = RecommendedVacationUiModel.INITIAL_DATA.copy(
+                    recommendedContent = "나는 바보입니다",
+                    vacationType = RecommendVacationType.LOCAL,
+                ),
+            )
+        }
         reduce { copy(isLoading = false) }
     }
 }
