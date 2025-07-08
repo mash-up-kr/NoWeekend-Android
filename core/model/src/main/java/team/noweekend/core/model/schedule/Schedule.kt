f649e9c71cf034636e76e82d2a40dcb996f811f0
Boris-xyz <boris@xyzcorp.io>
2025-07-08 17:49:38 +0900

    [#88] model 주석 추가

--- core/model/src/main/java/team/noweekend/core/model/schedule/Schedule.kt
+++ core/model/src/main/java/team/noweekend/core/model/schedule/Schedule.kt
@@ -2,14 +2,44 @@ package team.noweekend.core.model.schedule
 
 import team.noweekend.core.model.alarm.AlarmOption
 
+/**
+ * 스케줄 정보
+ */
 data class Schedule(
+    /**
+     * 스케줄 구분 id
+     */
     val id: String,
+    /**
+     * 스케줄 제목
+     */
     val title: String,
+    /**
+     * 스케줄 시작 시간
+     */
     val startTime: String,
+    /**
+     * 스케줄 종료 시간
+     */
     val endTime: String,
+    /**
+     * 스케줄 타입 [ScheduleCategory] 중 하나
+     */
     val category: ScheduleCategory,
+    /**
+     * 열정 온도
+     */
     val temperature: Int,
+    /**
+     * 종일 여부
+     */
     val allDay: Boolean,
+    /**
+     * 완료 여부
+     */
     val completed: Boolean,
+    /**
+     * 스케줄 알림 옵션 [AlarmOption] 중 하나
+     */
     val alarmOption: AlarmOption,
 )
--- core/model/src/main/java/team/noweekend/core/model/schedule/ScheduleCategory.kt
+++ core/model/src/main/java/team/noweekend/core/model/schedule/ScheduleCategory.kt
@@ -1,9 +1,27 @@
 package team.noweekend.core.model.schedule
 
+/**
+ * 스케줄 타입
+ */
 enum class ScheduleCategory {
+    /**
+     * 회사 일정
+     */
     COMPANY,
+
+    /**
+     * 개인 일정
+     */
     PERSONAL,
+
+    /**
+     * 기타 일정
+     */
     ETC,
+
+    /**
+     * 휴가 일정
+     */
     LEAVE,
     ;
 }
