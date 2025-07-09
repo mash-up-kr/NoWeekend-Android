152640db31e69d15ceb2b455155badaab8090167
JaesungLeee <viki0114@naver.com>
2025-07-09 17:16:27 +0900

    [#49] 사용자 정보 입력 스크린 구성

--- feature/onboarding/src/main/java/team/noweekend/feature/onboarding/profile/screen/ProfileInputScreen.kt
+++ feature/onboarding/src/main/java/team/noweekend/feature/onboarding/profile/screen/ProfileInputScreen.kt
@@ -7,11 +7,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.focus.FocusManager
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,9 +26,12 @@ import androidx.compose.ui.unit.dp
 import team.noweekend.core.design.system.core.component.button.defaults.BoxButtonType
 import team.noweekend.core.design.system.core.component.button.fill.NWKFillButton
 import team.noweekend.core.design.system.core.component.header.NWKHeader
+import team.noweekend.core.design.system.core.component.input.status.InputFieldStatus
 import team.noweekend.core.design.system.core.component.scaffold.NWKScaffold
 import team.noweekend.core.design.system.foundation.theme.NWKTheme
 import team.noweekend.core.resource.NWKStringResource
+import team.noweekend.feature.onboarding.profile.component.BirthInputField
+import team.noweekend.feature.onboarding.profile.component.NickNameInputField
 import team.noweekend.feature.onboarding.profile.component.ProfileInputScreenHeader
 
 @Composable
@@ -65,7 +75,18 @@ fun ProfileInputScreen(
 @Composable
 private fun ProfileInputScreenContent(
     modifier: Modifier = Modifier,
+    nickName: TextFieldState = TextFieldState(),
+    birth: TextFieldState = TextFieldState(),
+    nickNameInputFieldStatus: InputFieldStatus = InputFieldStatus.DEFAULT,
+    birthInputFieldStatus: InputFieldStatus = InputFieldStatus.DEFAULT,
 ) {
+    val focusManager: FocusManager = LocalFocusManager.current
+    val focusRequester = remember { FocusRequester() }
+
+    LaunchedEffect(key1 = Unit) {
+        focusRequester.requestFocus()
+    }
+
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -75,8 +96,18 @@ private fun ProfileInputScreenContent(
         Spacer(modifier = Modifier.size(48.dp))
         ProfileInputScreenHeader()
         Spacer(modifier = Modifier.size(NWKTheme.spacing.space500))
-
-
+        NickNameInputField(
+            textFieldState = nickName,
+            inputFieldStatus = nickNameInputFieldStatus,
+            focusRequester = focusRequester,
+            onKeyboardAction = { focusManager.moveFocus(FocusDirection.Down) },
+        )
+        Spacer(modifier = Modifier.size(24.dp))
+        BirthInputField(
+            textFieldState = birth,
+            inputFieldStatus = birthInputFieldStatus,
+            onKeyboardAction = { focusManager.clearFocus() },
+        )
     }
 }
 
