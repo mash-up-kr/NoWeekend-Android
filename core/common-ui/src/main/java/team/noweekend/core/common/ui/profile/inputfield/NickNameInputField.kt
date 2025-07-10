3e43669d26bfeeb5be689f6729f6bd8151e4080b
JaesungLeee <viki0114@naver.com>
2025-07-10 15:54:03 +0900

    [#95] string 리소스 정리

--- core/common-ui/src/main/java/team/noweekend/core/common/ui/profile/inputfield/NickNameInputField.kt
+++ core/common-ui/src/main/java/team/noweekend/core/common/ui/profile/inputfield/NickNameInputField.kt
@@ -5,10 +5,12 @@ import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import team.noweekend.core.design.system.core.component.input.NWKInputField
 import team.noweekend.core.design.system.core.component.input.status.InputFieldStatus
 import team.noweekend.core.design.system.core.component.input.status.TextInputType
+import team.noweekend.core.resource.NWKStringResource
 
 @Composable
 internal fun NickNameInputField(
@@ -19,9 +21,9 @@ internal fun NickNameInputField(
     modifier: Modifier = Modifier,
 ) {
     NWKInputField(
-        label = "닉네임",
-        placeholder = "1~6자 이내, 영문 숫자 사용 가능",
-        errorText = "6글까지 작성할 수 있어요.",
+        label = stringResource(NWKStringResource.NicknameInputLabel),
+        placeholder = stringResource(NWKStringResource.NicknameInputPlaceholder),
+        errorText = stringResource(NWKStringResource.NicknameInputErrorMessage),
         textFieldState = textFieldState,
         onKeyboardAction = onKeyboardAction,
         modifier = modifier.fillMaxWidth(),
