0fe31e85c68ef317008546a4d2f855425b96f612
JaesungLeee <viki0114@naver.com>
2025-07-10 18:00:27 +0900

    [#95] @014967 리뷰 반영 : remember

--- core/common-ui/src/main/java/team/noweekend/core/common/ui/profile/input/NickNameInputField.kt
+++ core/common-ui/src/main/java/team/noweekend/core/common/ui/profile/input/NickNameInputField.kt
@@ -3,6 +3,7 @@ package team.noweekend.core.common.ui.profile.input
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.res.stringResource
@@ -16,9 +17,9 @@ import team.noweekend.core.resource.NWKStringResource
 fun NickNameInputField(
     textFieldState: TextFieldState,
     onKeyboardAction: () -> Unit,
-    focusRequester: FocusRequester,
     inputFieldStatus: InputFieldStatus,
     modifier: Modifier = Modifier,
+    focusRequester: FocusRequester = remember { FocusRequester() },
 ) {
     NWKInputField(
         label = stringResource(NWKStringResource.NicknameInputLabel),
