771932ad1b1d3c3ac9e8a802c381a511d835ef4d
seojeongwoo <qw4643987@naver.com>
2025-07-13 19:59:15 +0900

    [#76] 베이스 에러 파서 구현

---
+++ core/remote/src/main/kotlin/team/noweekend/core/remote/base/BaseErrorPaser.kt
@@ -0,0 +1,41 @@
+import io.ktor.client.plugins.ResponseException
+import io.ktor.client.statement.HttpResponse
+import io.ktor.client.statement.bodyAsText
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.decodeFromJsonElement
+import kotlinx.serialization.json.jsonNull
+import kotlinx.serialization.json.jsonObject
+import team.noweekend.core.remote.base.NWKApiException
+import team.noweekend.core.remote.base.NWKErrorResponse
+
+object NWKApiErrorParser {
+    private val json = Json { ignoreUnknownKeys = true }
+
+    suspend fun parse(e: Throwable): Throwable {
+        return when (e) {
+            is ResponseException -> {
+                val response = e.response
+                val error = parseErrorResponse(response)
+                NWKApiException(errorResponse = error)
+            }
+
+            else -> e
+        }
+    }
+
+    private suspend fun parseErrorResponse(response: HttpResponse): NWKErrorResponse? {
+        return try {
+            val responseString = response.bodyAsText()
+            val root = json.parseToJsonElement(responseString).jsonObject
+
+            if (root["error"]?.jsonNull != null) return null
+
+            val errorElement = root["errorResponse"]
+            errorElement?.let {
+                json.decodeFromJsonElement<NWKErrorResponse>(it)
+            }
+        } catch (e: Exception) {
+            null
+        }
+    }
+}
