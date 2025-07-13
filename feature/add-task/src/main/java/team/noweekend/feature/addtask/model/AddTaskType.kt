11ba4d0295ba7fb4c20a5758749b620c9b046bfa
seojeongwoo <qw4643987@naver.com>
2025-07-14 00:53:40 +0900

    [#52] 패키지이동

--- feature/add-task/src/main/java/team/noweekend/feature/addtask/model/AddTaskType.kt
+++
@@ -1,9 +0,0 @@
-package team.noweekend.feature.addtask.model
-
-enum class AddTaskType(val tag: String) {
-    COMPANY(tag = "회사"),
-    PERSONAL(tag = "개인"),
-    OTHER(tag = "기타"),
-    VACATION(tag = "연차"),
-    ;
-}
