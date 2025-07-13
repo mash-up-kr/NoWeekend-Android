72d9ba38f406ec1d698ee73e0f5ae1a02b9fce4d
Boris-xyz <boris@xyzcorp.io>
2025-07-14 00:39:16 +0900

    [#76] 린트 적용

--- feature/profile/src/main/kotlin/team/noweekend/feature/profile/editInfo/screen/ProfileEditInfoScreen.kt
+++ feature/profile/src/main/kotlin/team/noweekend/feature/profile/editInfo/screen/ProfileEditInfoScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
@@ -71,7 +70,9 @@ internal fun ProfileEditInfoScreen(
         },
     ) { paddingValues ->
         MenuLayout(
-            modifier = Modifier.fillMaxSize().padding(paddingValues),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
             menuList = editInfoMenus,
             onClickMenuItem = {},
         )
