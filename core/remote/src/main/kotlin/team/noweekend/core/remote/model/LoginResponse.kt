05dda1c8c7cc12ba94690fc5229e049afab9c805
seojeongwoo <qw4643987@naver.com>
2025-07-13 22:45:15 +0900

    [#109] 로그인 리스폰스 모델 구현

---
+++ core/remote/src/main/kotlin/team/noweekend/core/remote/model/LoginResponse.kt
@@ -0,0 +1,12 @@
+package team.noweekend.core.remote.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class LoginResponse(
+    @SerialName("exists")
+    val exists: Boolean,
+    @SerialName("accessToken")
+    val accessToken: String,
+)
