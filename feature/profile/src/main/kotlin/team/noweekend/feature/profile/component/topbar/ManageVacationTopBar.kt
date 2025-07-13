2a256e792646afb244383fd8fe294fef6dc1051d
Boris-xyz <boris@xyzcorp.io>
2025-07-13 17:39:40 +0900

    [#107] 연차 관리 topBar추가

---
+++ feature/profile/src/main/kotlin/team/noweekend/feature/profile/component/topbar/ManageVacationTopBar.kt
@@ -0,0 +1,53 @@
+package team.noweekend.feature.profile.component.topbar
+
+import androidx.compose.foundation.clickable
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import team.noweekend.core.design.system.core.component.icon.NWKIcon
+import team.noweekend.core.design.system.foundation.theme.NWKTheme
+import team.noweekend.core.resource.NWKDrawableResource.ChevronLeft
+import team.noweekend.core.resource.NWKStringResource.ManageVacation
+import team.noweekend.core.resource.NWKStringResource.ProfileSaveButtonTitle
+import team.noweekend.feature.profile.component.topbar.core.Header
+import team.noweekend.feature.profile.component.topbar.core.HeaderAlignmentType
+
+@Composable
+fun ManageVacationTopBar(
+    onClickSaveButton: () -> Unit,
+    onClickBackButton: () -> Unit,
+    modifier: Modifier = Modifier,
+
+    ) {
+    Header(
+        modifier = modifier,
+        components = arrayOf(
+            { componentModifier ->
+                NWKIcon(
+                    modifier = componentModifier.clickable(onClick = onClickBackButton),
+                    resourceId = ChevronLeft,
+                )
+            },
+            { componentModifier ->
+                Text(
+                    modifier = componentModifier,
+                    text = stringResource(id = ManageVacation),
+                    style = NWKTheme.typography.heading6,
+                    color = NWKTheme.color.Semantic.Text.neutral,
+                    textAlign = TextAlign.Center,
+                )
+            },
+            { componentModifier ->
+                Text(
+                    modifier = componentModifier.clickable(onClick = onClickSaveButton),
+                    text = stringResource(id = ProfileSaveButtonTitle),
+                    style = NWKTheme.typography.heading6,
+                    color = NWKTheme.color.Toast.toast500,
+                )
+            },
+        ),
+        alignment = HeaderAlignmentType.CenterFill,
+    )
+}
