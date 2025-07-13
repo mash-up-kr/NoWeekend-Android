d693f8f5ca8291f45f51b39c3e25ed349577721c
seojeongwoo <qw4643987@naver.com>
2025-07-13 19:59:44 +0900

    [#76] 베이스 리스폰스 적용

--- core/remote/src/main/kotlin/team/noweekend/core/remote/base/BaseApiCall.kt
+++ core/remote/src/main/kotlin/team/noweekend/core/remote/base/BaseApiCall.kt
@@ -11,37 +11,46 @@ import io.ktor.client.request.setBody
 internal suspend inline fun <reified T> HttpClient.getApiCall(
     path: String,
     queries: Map<String, String>? = null,
-): T = this
-    .get(path) {
-        queries?.forEach { (key, value) ->
-            parameter(key, value)
-        }
-    }
-    .body()
+): T =
+    callApi<NWKResponse<T>> {
+        this
+            .get(path) {
+                queries?.forEach { (key, value) ->
+                    parameter(key, value)
+                }
+            }
+            .body()
+    }.data
 
 
 internal suspend inline fun <reified T> HttpClient.postApiCall(
     path: String,
     body: Any? = null,
     queries: Map<String, String>? = null,
-): T = this
-    .post(path) {
-        body?.let { setBody(body) }
-        queries?.forEach { (key, value) ->
-            parameter(key, value)
-        }
-    }
-    .body()
+): T =
+    callApi<NWKResponse<T>> {
+        this
+            .post(path) {
+                body?.let { setBody(body) }
+                queries?.forEach { (key, value) ->
+                    parameter(key, value)
+                }
+            }
+            .body()
+    }.data
 
 internal suspend inline fun <reified T> HttpClient.putApiCall(
     path: String,
     body: Any? = null,
     queries: Map<String, String>? = null,
-): T = this
-    .put(path) {
-        body?.let { setBody(body) }
-        queries?.forEach { (key, value) ->
-            parameter(key, value)
-        }
-    }
-    .body()
+): T =
+    callApi<NWKResponse<T>> {
+        this
+            .put(path) {
+                body?.let { setBody(body) }
+                queries?.forEach { (key, value) ->
+                    parameter(key, value)
+                }
+            }
+            .body()
+    }.data
