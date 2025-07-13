12496f27e4cb13f83755d28d6d9769f232e33863
seojeongwoo <qw4643987@naver.com>
2025-07-13 20:00:14 +0900

    [#76] serialization 플러그인 추가

--- build-logic/convention/src/main/java/AndroidLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/java/AndroidLibraryConventionPlugin.kt
@@ -13,8 +13,9 @@ class AndroidLibraryConventionPlugin : Plugin<Project> {
             with(pluginManager) {
                 apply("com.android.library")
                 apply("org.jetbrains.kotlin.android")
+                apply("org.jetbrains.kotlin.plugin.serialization")
             }
-            
+
             extensions.configure<LibraryExtension> {
                 configureKotlinAndroid(this)
                 configureBuildFlavors(this@with)
