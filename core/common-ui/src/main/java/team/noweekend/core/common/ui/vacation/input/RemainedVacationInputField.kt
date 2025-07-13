983441821f53562d3946607fad768f7b952baaac
Boris-xyz <boris@xyzcorp.io>
2025-07-13 17:37:30 +0900

    [#107] errorText와 inputFieldStatus를 parameter에 추가

--- core/common-ui/src/main/java/team/noweekend/core/common/ui/vacation/input/RemainedVacationInputField.kt
+++ core/common-ui/src/main/java/team/noweekend/core/common/ui/vacation/input/RemainedVacationInputField.kt
@@ -9,6 +9,7 @@ import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import team.noweekend.core.design.system.core.component.input.NWKInputField
+import team.noweekend.core.design.system.core.component.input.status.InputFieldStatus
 import team.noweekend.core.design.system.core.component.input.status.TextInputType
 import team.noweekend.core.resource.NWKStringResource
 
@@ -17,17 +18,20 @@ fun RemainedVacationInputField(
     vacationState: TextFieldState,
     onKeyboardAction: () -> Unit,
     modifier: Modifier = Modifier,
+    errorText: String = "",
+    inputTextFieldStatus: InputFieldStatus = InputFieldStatus.DEFAULT,
     focusRequester: FocusRequester = remember { FocusRequester() },
 ) {
     NWKInputField(
         label = stringResource(NWKStringResource.RemainedVacationInputLabel),
         placeholder = stringResource(NWKStringResource.RemainedVacationInputPlaceholder),
-        errorText = "",
+        errorText = errorText,
         textFieldState = vacationState,
         onKeyboardAction = onKeyboardAction,
         modifier = modifier.fillMaxWidth(),
         focusRequester = focusRequester,
         textInputType = TextInputType.DAY,
+        inputFieldStatus = inputTextFieldStatus,
         keyboardImeAction = ImeAction.Done,
     )
 }
