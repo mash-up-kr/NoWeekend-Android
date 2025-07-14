17c90ce133714b28a57023aff14b3f95ee374739
SEO-J17 <qw4643987@naver.com>
2025-07-15 01:19:48 +0900

    [#52] LocalTime 포매터 수정

--- core/common-kotlin/src/main/java/team/noweekend/core/common/kotlin/extension/LocalTime.kt
+++ core/common-kotlin/src/main/java/team/noweekend/core/common/kotlin/extension/LocalTime.kt
@@ -3,11 +3,15 @@ package team.noweekend.core.common.kotlin.extension
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.LocalTime
 import kotlinx.datetime.toJavaLocalDateTime
+import kotlinx.datetime.toJavaLocalTime
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 
 val LocalTime.Companion.MERIDIEM_HOUR_MINUTE_KR_PATTERN
     get() = "(a) HH:mm"
+
+val LocalTime.Companion.KOREAN_MERIDIEM_HOUR_MINUTE_PATTERN: String
+    get() = "a h:mm"
 
 fun LocalDateTime.toFormattedString(pattern: String): String {
     val javaLocalDateTime = this.toJavaLocalDateTime()
@@ -15,4 +19,11 @@ fun LocalDateTime.toFormattedString(pattern: String): String {
         ZoneId.systemDefault(),
     )
     return javaLocalDateTime.format(formatter)
+}
+
+fun LocalTime.toFormattedString(pattern: String): String {
+    val javaLocalTime = this.toJavaLocalTime()
+    val formatter = DateTimeFormatter.ofPattern(pattern)
+        .withZone(ZoneId.systemDefault())
+    return javaLocalTime.format(formatter)
 }
