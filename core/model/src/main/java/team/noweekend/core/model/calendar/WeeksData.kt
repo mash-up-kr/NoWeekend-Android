e1428eb5785d7751f343019bbb8ae1f325cedaa5
Boris-xyz <boris@xyzcorp.io>
2025-07-15 14:20:48 +0900

    [#120] 모델 수정 및 주석 제거

--- core/model/src/main/java/team/noweekend/core/model/calendar/WeeksData.kt
+++ core/model/src/main/java/team/noweekend/core/model/calendar/WeeksData.kt
@@ -7,15 +7,4 @@ data class WeeksData(
      * 월과 주의 데이터 관리를 위해 2차원 List 로 관리
      */
     val dateOfWeeks: List<List<DateOfWeek>>,
-) {
-//    companion object {
-//
-//        private val now = LocalDate.now()
-//
-//        val default = WeeksData(
-//            year = now.year,
-//            month = now.monthNumber,
-//            dateOfWeeks = persistentListOf(),
-//        )
-//    }
-}
+)
