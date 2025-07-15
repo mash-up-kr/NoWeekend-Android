d51d6a4d1d2dd4ef078840e220ab90acf5bcb577
SEO-J17 <qw4643987@naver.com>
2025-07-15 16:53:03 +0900

    [#109] 린트 적용

--- core/data/src/main/java/team/noweekend/data/di/RepositoryModule.kt
+++ core/data/src/main/java/team/noweekend/data/di/RepositoryModule.kt
@@ -5,12 +5,12 @@ import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import team.noweekend.core.domain.repository.AuthRepository
-import team.noweekend.core.domain.repository.LoginRepository
 import team.noweekend.core.domain.repository.HolidayRepository
+import team.noweekend.core.domain.repository.LoginRepository
 import team.noweekend.core.domain.repository.ScheduleRepository
 import team.noweekend.data.repository.AuthRepositoryImpl
-import team.noweekend.data.repository.LoginRepositoryImpl
 import team.noweekend.data.repository.HolidayRepositoryImpl
+import team.noweekend.data.repository.LoginRepositoryImpl
 import team.noweekend.data.repository.ScheduleRepositoryImpl
 import javax.inject.Singleton
 
