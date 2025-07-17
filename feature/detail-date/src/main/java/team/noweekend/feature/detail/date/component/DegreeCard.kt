caf0a1d0cf2443b4bb61d17725a3d9765991db57
Boris-xyz <boris@xyzcorp.io>
2025-07-17 22:41:42 +0900

    [#147] 이미지 조정

--- feature/detail-date/src/main/java/team/noweekend/feature/detail/date/component/DegreeCard.kt
+++ feature/detail-date/src/main/java/team/noweekend/feature/detail/date/component/DegreeCard.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.FirstBaseline
 import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.layout.Measurable
