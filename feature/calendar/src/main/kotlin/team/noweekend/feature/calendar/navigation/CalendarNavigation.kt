3eb60f92ffd3f0b7be68f8e69025a4aff68c9abc
JaesungLeee <viki0114@naver.com>
2025-07-18 00:40:57 +0900

    compose lint 수정

--- feature/calendar/src/main/kotlin/team/noweekend/feature/calendar/navigation/CalendarNavigation.kt
+++ feature/calendar/src/main/kotlin/team/noweekend/feature/calendar/navigation/CalendarNavigation.kt
@@ -1,17 +1,11 @@
 package team.noweekend.feature.calendar.navigation
 
 import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import kotlinx.serialization.InternalSerializationApi
 import team.noweekend.core.common.ui.todo.model.Todo
 import team.noweekend.core.navigator.model.Calendar
 import team.noweekend.feature.calendar.screen.CalendarRoute
-
-@OptIn(InternalSerializationApi::class)
-fun NavHostController.navigateToCalendar(date: String) {
-    navigate(Calendar)
-}
 
 @OptIn(InternalSerializationApi::class)
 fun NavGraphBuilder.calendarNavGraph(
