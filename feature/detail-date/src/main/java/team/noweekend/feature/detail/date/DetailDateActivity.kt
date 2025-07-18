1b0b627473f0168297d13bba438568a1b90e34e8
seojeongwoo <qw4643987@naver.com>
2025-07-18 18:44:05 +0900

    [#165] 할 일 추가,수정 화면 이동 로직 추가

--- feature/detail-date/src/main/java/team/noweekend/feature/detail/date/DetailDateActivity.kt
+++ feature/detail-date/src/main/java/team/noweekend/feature/detail/date/DetailDateActivity.kt
@@ -7,12 +7,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.serialization.json.Json
+import team.noweekend.core.common.android.mvi.Intent
 import team.noweekend.core.design.system.foundation.theme.NWKTheme
+import team.noweekend.core.navigator.delegate.CalendarNavigationDelegate
 import team.noweekend.core.navigator.model.DetailDate
 import team.noweekend.feature.detail.date.navigation.DetailDateNavHost
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class DetailDateActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var calendarNavigationDelegate: CalendarNavigationDelegate
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,9 +33,30 @@ class DetailDateActivity : ComponentActivity() {
                     startDestination = DetailDate(date = date, todoList = todoList),
                     modifier = Modifier.fillMaxSize(),
                     navigateToBack = ::finish,
-                    navigateToAddTodo = {},
-                    navigateToEditTodo = {},
-                    navigateToAddTodoWithDirectInput = {},
+                    navigateToAddTodo = {
+                        calendarNavigationDelegate.navigateToAddTask(
+                            activity = this,
+                            launcher = null,
+                        )
+                    },
+                    navigateToEditTodo = {
+                        val json = Json.encodeToString(it)
+                        calendarNavigationDelegate.navigateToAddTask(
+                            activity = this,
+                            intentBuilder = {
+                                android.content.Intent().apply {
+                                    putExtra("id", json)
+                                }
+                            },
+                            launcher = null,
+                        )
+                    },
+                    navigateToAddTodoWithDirectInput = {
+                        calendarNavigationDelegate.navigateToAddTask(
+                            activity = this,
+                            launcher = null,
+                        )
+                    },
                 )
             }
         }
